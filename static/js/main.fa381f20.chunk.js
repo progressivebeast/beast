c8cabf37ec79b386fe149fae587f48885e7da1fb
progressivebeast <progressivebeast@gmail.com>
2020-11-07 11:48:38 +0100

    Updates

--- static/js/main.fa381f20.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpbeast=this.webpackJsonpbeast||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n(0),s=n.n(r),a=n(3),i=function(){return Object(c.jsx)("div",{children:"Learn React"})};n.n(a).a.render(Object(c.jsx)(s.a.StrictMode,{children:Object(c.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.fa381f20.chunk.js.map
